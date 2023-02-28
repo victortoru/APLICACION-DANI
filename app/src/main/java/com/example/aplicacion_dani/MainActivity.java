9bd6ae60a8f10fb43d77ed353a7c8ae0cdf9b002
victortoru <victortobaruelaruiz@elpuig.xeill.net>
2023-02-28 20:53:39 +0100

    Seguir con layouts y libreria de maps

--- app/src/main/java/com/example/aplicacion_dani/MainActivity.java
+++ app/src/main/java/com/example/aplicacion_dani/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.MapView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.setBuiltInZoomControls(true);
     }
 }
